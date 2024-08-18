d238175527bd085f6c3c2e9e7561bf827aa05e8c
nadjamladenovic <nadjamladenovic26@gmail.com>
2024-08-19 00:04:09 +0200

    nav i index

--- rasadnik-app/src/comp/nav.js
+++ rasadnik-app/src/comp/nav.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import {FaBeer} from 'react-icons/fa'
+
 import { MdLocalShipping } from "react-icons/md";
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FiLogIn } from 'react-icons/fi';
+import { useAuth0 } from "@auth0/auth0-react";
+const { loginWithRedirect } = useAuth0();
+
+
 import './nav.css';
-import { IoIosSearch } from "react-icons/io";
+
 
 const Nav = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
       <div className='header'>
         <div className='top_header'>
             <div className='icon'>
-            <MdLocalShipping />
+              <MdLocalShipping />
 
             </div>
             <div className='info'>
@@ -23,7 +30,16 @@ const Nav = () => {
             </div>
             <div className='search_box'>
                 <input type='text' value='' placeholder='search'></input>
-                <button><IoIosSearch/></button>
+                <button><AiOutlineSearch /></button>
+            </div>
+            <div className='user'>
+              <div className='icon'>
+                <FiLogIn />
+              </div>
+              <div className='btn'>
+                <button onClick={() => loginWithRedirect()}>Login</button>
+              </div>
+
             </div>
            
         </div>
