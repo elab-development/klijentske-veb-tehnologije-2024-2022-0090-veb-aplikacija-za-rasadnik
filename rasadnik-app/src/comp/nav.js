e263ebbd71351a20b3e9a83619ec5a4be6ff5a9b
Natalija Toncic <natalijatoncic123@gmail.com>
2024-08-19 18:55:48 +0200

    nav

--- rasadnik-app/src/comp/nav.js
+++ rasadnik-app/src/comp/nav.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { MdLocalShipping } from "react-icons/md";
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FiLogIn } from 'react-icons/fi';
-import { CiLogout } from 'react-icons/ci';
+import { CiLogout, CiUser } from 'react-icons/ci';
 import { useAuth0 } from "@auth0/auth0-react"; 
+import {Link} from 'react-router-dom';
 
 import './nav.css';
 
@@ -50,8 +51,45 @@ const Nav = () => {
             <div className='btn'>
               <button onClick={() => loginWithRedirect()}>Login</button>
             </div>
-        </div>
+          </div>
           }
+        </div>
+        <div className='last_header'>
+          <div className='user_profile'>
+            {
+              isAuthenticated ? 
+              <>
+              <div className='icon'>
+                <CiUser />
+              </div>
+              <div className='info'>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+              </div>
+              </>
+              :
+              <>
+              <div className='icon'>
+                <CiUser />
+              </div>
+              <div className='info'>
+                <p>Please login</p>
+              </div>
+              </>
+            }
+          </div>
+          <div className='nav'>
+            <ul>
+              <li><Link to='/' className='link'>Home</Link></li>
+              <li><Link to='/shop' className='link'>Shop</Link></li>  
+              <li><Link to='/collection' className='link'>Collection</Link></li>  
+              <li><Link to='/about' className='link'>About</Link></li>  
+              <li><Link to='/contact' className='link'>Contact</Link></li>                
+            </ul>
+          </div>
+          <div className='offer'>
+            <p>flat 10% over all plants</p>
+          </div>
         </div>
       </div>
     </>
