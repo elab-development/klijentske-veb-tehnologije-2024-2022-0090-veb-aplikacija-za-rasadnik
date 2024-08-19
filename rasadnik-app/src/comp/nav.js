89422aa08a0bb32b7ea08cd0b7bab9a016c80b41
Natalija Toncic <natalijatoncic123@gmail.com>
2024-08-19 17:07:10 +0200

    nav.js

--- rasadnik-app/src/comp/nav.js
+++ rasadnik-app/src/comp/nav.js
@@ -1,50 +1,46 @@
-import React from 'react'
-
+import React from 'react';
 import { MdLocalShipping } from "react-icons/md";
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FiLogIn } from 'react-icons/fi';
-import { useAuth0 } from "@auth0/auth0-react";
-const { loginWithRedirect } = useAuth0();
-
+import { useAuth0 } from "@auth0/auth0-react"; // Ovo treba da bude na vrhu
 
 import './nav.css';
 
-
 const Nav = () => {
+  // `useAuth0` mora biti pozvan unutar funkcionalne komponente
   const { loginWithRedirect } = useAuth0();
+
   return (
     <>
       <div className='header'>
         <div className='top_header'>
+          <div className='icon'>
+            <MdLocalShipping />
+          </div>
+          <div className='info'>
+            <p>Free Shipping When Shopping upto $1000</p>
+          </div>
+        </div>
+        <div className='mid_header'>
+          <div className='logo'>
+            <img src='image/image.logo.webp' alt='logo'></img>
+          </div>
+          <div className='search_box'>
+            <input type='text' placeholder='search'></input>
+            <button><AiOutlineSearch /></button>
+          </div>
+          <div className='user'>
             <div className='icon'>
-              <MdLocalShipping />
-
+              <FiLogIn />
             </div>
-            <div className='info'>
-                <p>Free Shipping When Shopping upto $1000</p>
+            <div className='btn'>
+              <button onClick={() => loginWithRedirect()}>Login</button>
             </div>
+          </div>
         </div>
-        <div className='mid_header'></div>
-            <div className='logo'>
-                <img src='image/image.logo.webp' alt='logo'></img>
-            </div>
-            <div className='search_box'>
-                <input type='text' value='' placeholder='search'></input>
-                <button><AiOutlineSearch /></button>
-            </div>
-            <div className='user'>
-              <div className='icon'>
-                <FiLogIn />
-              </div>
-              <div className='btn'>
-                <button onClick={() => loginWithRedirect()}>Login</button>
-              </div>
-
-            </div>
-           
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
