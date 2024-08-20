7d83e9d97352c1bfadbd62144a1d52bd048913f2
Natalija Toncic <natalijatoncic123@gmail.com>
2024-08-20 18:14:43 +0200

    commit

---
+++ rasadnik-app/src/comp/home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './home.css'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+    <div className='home'>
+        <div className='top_banner'>
+            <div className='contant'>
+                <h3>WELCOME TO PLANTBLOOM</h3>
+                <Link to='/shop' className='link'>Shop now</Link>
+            </div>
+        </div>   
+        <div className='tranding'>
+            <div className='container'>
+                <div className='left_box'>
+                    <div className='header'>
+                        <div className='heading'>
+                            <h2>trending product</h2>
+                        </div>
+                        <div className='cate'>
+                            <h3>New</h3>
+                            <h3>Featured</h3>
+                            <h3>Top selling</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='right_box'></div>
+            </div>
+        </div>     
+    </div>
+    </>
+  )
+}
+
+export default Home 
