0e4d6453a84f3105c1c4568191d173034b5c2ad0
nadjamladenovic <nadjamladenovic26@gmail.com>
2024-08-21 22:47:04 +0200

    footer.js i footer.css

--- rasadnik-app/src/App.js
+++ rasadnik-app/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Nav from './comp/nav'
 import {BrowserRouter} from 'react-router-dom'
 import Rout from './comp/rout'
-
+import Footer from './comp/footer'
 const App = () => {
   return (
     <>
       <BrowserRouter>
       <Nav />
       <Rout />
+      <Footer />
       </BrowserRouter>
       
     </>
