8ed93640b40e7cd57abb5a621c8b61851b7d20c4
Natalija Toncic <natalijatoncic123@gmail.com>
2024-08-23 17:09:33 +0200

    details

--- rasadnik-app/src/comp/shop.js
+++ rasadnik-app/src/comp/shop.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './shop.css'
-import { AiFillHeart, AiFillEye } from 'react-icons/ai';
+import { AiFillHeart, AiFillEye, AiOutlineClose } from 'react-icons/ai';
 import { MdCrueltyFree } from 'react-icons/md'
 import { FiFilter } from 'react-icons/fi';
 
 
 const Shop = ({shop, Filter, allcatefilter, addtocart}) => {
+
+    const [showDetail, setShowDetail] = useState(false)
+
+    const [detail, setDetail] = useState([])
+
+    const detailpage = (product) =>
+    {
+        const detaildata =  ([{product}])
+        const productdetail = detaildata[0]['product']
+        //console.log(productdetail)
+        setDetail(productdetail)
+        setShowDetail(true)
+    }
+    console.log(productdetail)
+    const closedetail = () =>
+    {
+        setShowDetail(false)
+    }
   return (
     <>
+    {
+        showDetail ?
+        <>
+        <div className='product_detail'>
+            <button className='close_btn' onClick={closedetail}><AiOutlineClose /></button>
+            <div className='container'>
+                <div className='img_box'>
+                    <img src={detail.images} alt=''></img>
+                </div>
+                <div className='info'>
+                    <h4># {detail.cat}</h4>
+                    <h2>{detail.Name}</h2>
+                    <p>NEKI TEKST</p>
+                    <button onClick={() => addtocart (detail)}>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+        </>
+        : null
+    }
+    
     <div className='shop'>
         <h2># Shop</h2>
         <p>Home . Shop</p>
@@ -55,7 +94,7 @@ const Shop = ({shop, Filter, allcatefilter, addtocart}) => {
                                         <img src={curElm.images} alt=''></img>
                                         <div className='icon'>
                                            <li><AiFillHeart /></li> 
-                                           <li><AiFillEye /></li>
+                                           <li onClick={() => detailpage (curElm)}><AiFillEye /></li>
                                         </div>
                                     </div>
                                     <div className='detail'>
