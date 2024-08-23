b436de66da625081bf651d34055feb05f45881bb
Natalija Toncic <natalijatoncic123@gmail.com>
2024-08-24 01:02:25 +0200

    dodatak u contact

--- rasadnik-app/src/comp/contact.js
+++ rasadnik-app/src/comp/contact.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contact.css'
 
 const Contact = () => {
+
+    const[user, setUser] = useState(
+        {
+            Name: '', Email: '', Subject: '', Message: ''
+        }
+    )
+        let values, names
+    const data = (e) =>
+    {
+        values = e.target.value 
+        names = e.target.name 
+        setUser({...user, [names]: values})
+    }
+
+    const send = async (e) =>
+    {
+        const {Name, Email, Subject, Message} = user
+        e.preventDefault()
+        const option = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'aplication/json'
+            },
+            body: JSON.stringify({
+                Name, Email, Subject, Message
+            })
+        }
+
+        const send = await fetch(
+            'https://rasadnik-app-default-rtdb.firebaseio.com/Message.json', option
+            )
+
+        if (send) {
+            alert("Message Sent")
+        }
+        else
+        {
+            alert("Error Occoured Message Sent failed")
+        }
+
+    }
+
   return (
     <>
     <div className='contact'>
@@ -16,7 +58,7 @@ const Contact = () => {
                             </h4>
                         </div>
                         <div className='input'>
-                            <intput type='text' placeholder='Name' value='' name=''></intput>
+                            <intput type='text' placeholder='Name' value={user.Name} name='Name' onChange={data}></intput>
                         </div>
                     </div>
                     <div className='box'>
@@ -26,7 +68,7 @@ const Contact = () => {
                             </h4>
                         </div>
                         <div className='input'>
-                            <intput type='email' placeholder='E-mail' value='' name=''></intput>
+                            <intput type='email' placeholder='Email' value={user.Email} name='Email' onChange={data}></intput>
                         </div>
                     </div>
                     <div className='box'>
@@ -36,7 +78,7 @@ const Contact = () => {
                             </h4>
                         </div>
                         <div className='input'>
-                            <intput type='text' placeholder='Subject' value='' name=''></intput>
+                            <intput type='text' placeholder='Subject' value={user.Subject} name='Subject' onChange={data}></intput>
                         </div>
                     </div>
                     <div className='box'>
@@ -46,10 +88,10 @@ const Contact = () => {
                             </h4>
                         </div>
                         <div className='input'>
-                            <textarea placeholder='Message !' value='' name=''></textarea>
+                            <textarea placeholder='Message !' value={user.Message} name='Message' onChange={data}></textarea>
                         </div>
                     </div>
-                    <button type='sublit'>Send</button>
+                    <button type='sublit' onClick={send}>Send</button>
                 </form>
             </div>
         </div>
