d6ecb3e4e9d8a23a53240b0e1a888314d9cd3445
tijanasikanja <tijanasikanja@gmail.com>
2024-09-01 15:20:29 +0200

    dugme show more

--- rasadnik-app/src/pages/home.tsx
+++ rasadnik-app/src/pages/home.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/home.css'
-import { createRoutesFromElements, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Homeproduct from '../comp/home_product'
 import { FaEye, FaHeart } from "react-icons/fa";
 import { LuFacebook } from "react-icons/lu";
 import { FaInstagram } from "react-icons/fa";
 import { PiTiktokLogo } from "react-icons/pi";
-import { AiFillEye, AiFillHeart, AiOutlineYoutube,AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineYoutube } from "react-icons/ai";
 
 interface Product {
     id: number;
@@ -20,21 +20,22 @@ interface HomeProps {
     addtocart: (product: Product) => void;
 }
 
-
-
 const Home: React.FC<HomeProps> = ({ addtocart }) => {
     const [newProduct, setNewProduct] = useState<Product[]>([]);
     const [featuredProduct, setFeaturedProduct] = useState<Product[]>([]);
     const [topProduct, setTopProduct] = useState<Product[]>([]);
     const [trendingProduct, setTrendingproduct] = useState<Product[]>(Homeproduct);
+    const [visibleProducts, setVisibleProducts] = useState<number>(12);
 
     const filtercate = (type: string) => {
         const filterproduct = Homeproduct.filter(curElm => curElm.type === type);
         setTrendingproduct(filterproduct);
+        setVisibleProducts(12); // Resetovanje na početnih 12 proizvoda pri promeni filtera
     };
 
     const allTrendingProduct = () => {
         setTrendingproduct(Homeproduct);
+        setVisibleProducts(12); // Resetovanje na početnih 12 proizvoda pri prikazu svih
     };
 
     useEffect(() => {
@@ -51,6 +52,11 @@ const Home: React.FC<HomeProps> = ({ addtocart }) => {
         const topcategory = Homeproduct.filter(x => x.type === 'top');
         setTopProduct(topcategory);
     };
+
+    const handleShowMore = () => {
+        setVisibleProducts(prev => prev + 12); // Povećanje broja prikazanih proizvoda za 12
+    };
+
     return (
     <>
     <div className='home'>
@@ -65,50 +71,51 @@ const Home: React.FC<HomeProps> = ({ addtocart }) => {
                 <div className='left_box'>
                     <div className='header'>
                         <div className='heading'>
-                            <h2 onClick={()=> allTrendingProduct ()}>Trending product</h2>
+                            <h2 onClick={() => allTrendingProduct()}>Trending product</h2>
                         </div>
                         <div className='cate'>
-                            <h3 onClick={()=> filtercate ('new')}>New</h3>
-                            <h3 onClick={()=> filtercate ('featured')}>Featured</h3>
-                            <h3 onClick={()=> filtercate ('top')}>Top selling</h3>
+                            <h3 onClick={() => filtercate('new')}>New</h3>
+                            <h3 onClick={() => filtercate('featured')}>Featured</h3>
+                            <h3 onClick={() => filtercate('top')}>Top selling</h3>
                         </div>
                     </div>
                     <div className='products'>
                         <div className='container'>
                             {
-                                trendingProduct.map((curElm) =>
+                                trendingProduct.slice(0, visibleProducts).map((curElm) =>
                                 {
-                                    return(
-                                        <>
-                                        <div className='box'>
+                                    return (
+                                        <div className='box' key={curElm.id}>
                                             <div className='img_box'>
-                                                <img src={curElm.images} alt=''></img>
+                                                <img src={curElm.images} alt='' />
                                                 <div className='icon'>
                                                     <div className='icon_box'>
-                                                    <FaEye />
+                                                        <FaEye />
                                                     </div>
                                                     <div className='icon_box'>
-                                                    <FaHeart />
+                                                        <FaHeart />
                                                     </div>
                                                 </div>
                                             </div>
                                             <div className='info'>
                                                 <h3>{curElm.Name}</h3>
                                                 <p>{curElm.price}</p>
-                                                <button className='btn' onClick={() => addtocart (curElm)}>Add To Cart</button>
+                                                <button className='btn' onClick={() => addtocart(curElm)}>Add To Cart</button>
                                             </div>
                                         </div>
-                                        </>
                                     )
                                 })
                             }
                         </div>
-                        <button>Show More</button>
+                        {
+                            visibleProducts < trendingProduct.length && (
+                                <button onClick={handleShowMore}>Show More</button>
+                            )
+                        }
                     </div>
                 </div>
                 <div className='right_box'>
                     <div className='right_container'>
-                      
                         <div className='newsletter'>
                             <div className='head'>
                                 <h3>Newsletter</h3>
@@ -136,11 +143,9 @@ const Home: React.FC<HomeProps> = ({ addtocart }) => {
                 </div>
             </div>
         </div>  
-
-
     </div>
     </>
   )
 }
 
-export default Home 
+export default Home;
